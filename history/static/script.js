a797a354dc2ad48dbbb1abfdb7d6572237dab3b6
Nguyễn Huy Nghĩa <116424780+trongnghia2007@users.noreply.github.com>
2024-06-24 11:49:44 +0700

    Update script.js

--- history/static/script.js
+++ history/static/script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
           imgDiv.classList.add('khung');
 
           const imgElement = document.createElement('img');
-          imgElement.src = `/images/history/${image.filename}`;
+          imgElement.src = image.path;
           imgElement.alt = "Image";
           imgElement.classList.add('khung-img');
           imgDiv.appendChild(imgElement);
@@ -40,13 +40,16 @@ document.addEventListener("DOMContentLoaded", function() {
           imgDiv.addEventListener('click', function() {
             const resultDiv = document.getElementById(`result-${index}`);
             if (resultDiv.style.display === 'none' || resultDiv.style.display === '') {
-              resultDiv.style.display = 'block';
+              resultDiv.style.display = 'flex';
             } else {
               resultDiv.style.display = 'none';
             }
           });
         } 
+
+ // ======================================================================================================= //
         
+
         else if (image.folder === 'test') {
           // Thêm ảnh vào resultlist với display=none
           const resultDiv = document.createElement('div');
@@ -59,7 +62,7 @@ document.addEventListener("DOMContentLoaded", function() {
           resultDiv.appendChild(frameDiv);
 
           const resultImg = document.createElement('img');
-          resultImg.src = `/images/test/${image.filename}`;
+          resultImg.src = image.path;
           resultImg.alt = "Result Image";
           resultImg.classList.add('frame-img');
           frameDiv.appendChild(resultImg);
@@ -67,8 +70,38 @@ document.addEventListener("DOMContentLoaded", function() {
           resultDiv.style.display = 'none';
 
           resultIndex++;
+        } 
+        
+        else if (image.folder === 'segments') {
+          const segmentIndex = parseInt(image.subfolder, 10);
+          let resultDiv = document.getElementById(`result-${segmentIndex}`);
+
+          if (!resultDiv) {
+            resultDiv = document.createElement('div');
+            resultDiv.id = `result-${segmentIndex}`;
+            resultDiv.classList.add('result');
+            resultlist.appendChild(resultDiv);
+          }
+
+          const frameDiv = document.createElement('div');
+          frameDiv.classList.add('frame-sm');
+          resultDiv.appendChild(frameDiv);
+
+          const resultImg = document.createElement('img');
+          resultImg.src = image.path;
+          resultImg.alt = "Segment Image";
+          resultImg.classList.add('frame-sm-img');
+          frameDiv.appendChild(resultImg);
+
+          const description = document.createElement('p');
+          description.textContent = image.filename.split('-')[1];
+          description.textContent = description.textContent.split('.')[0];
+          frameDiv.appendChild(description);
+
+          resultDiv.style.display = 'none';
         }
       });
     })
+    
     .catch(error => console.error('Error fetching images:', error));
 });
