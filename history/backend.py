a9515d95b1e4941156e22e60cd6ecf625ae38235
Nguyễn Huy Nghĩa <116424780+trongnghia2007@users.noreply.github.com>
2024-06-24 11:48:52 +0700

    Update backend.py

--- history/backend.py
+++ history/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_from_directory, render_template, request
+from flask import Flask, jsonify, send_from_directory, render_template
 import os
 from datetime import datetime
 
@@ -8,9 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template("history.html")
 
-# Path to the folder containing medical images
+# Path to the folders containing images
 HISTORY_FOLDER = os.path.join(app.root_path, 'history')
 TEST_FOLDER = os.path.join(app.root_path, 'test')
+SEGMENTS_FOLDER = os.path.join(app.root_path, 'segments')
 
 @app.route('/images', methods=['GET'])
 def list_images():
@@ -23,12 +24,28 @@ def list_images():
                 file_info = os.stat(file_path)
                 modification_time = datetime.fromtimestamp(file_info.st_mtime).strftime('%d/%m/%Y')
                 images.append({'filename': filename, 'date': modification_time, 'folder': folder_name, 'path': f'/images/{folder_name}/{filename}'})
+    
+    for subdir in os.listdir(SEGMENTS_FOLDER):
+        subdir_path = os.path.join(SEGMENTS_FOLDER, subdir)
+        if os.path.isdir(subdir_path):
+            for filename in os.listdir(subdir_path):
+                if filename.endswith(('.jpg', '.jpeg', '.png')):
+                    file_path = os.path.join(subdir_path, filename)
+                    file_info = os.stat(file_path)
+                    modification_time = datetime.fromtimestamp(file_info.st_mtime).strftime('%d/%m/%Y')
+                    images.append({'filename': filename, 'date': modification_time, 'folder': 'segments', 'subfolder': subdir, 'path': f'/images/segments/{subdir}/{filename}'})
+    
     print(images)
     return jsonify(images)
 
 @app.route('/images/<folder>/<filename>', methods=['GET'])
-def get_image(folder, filename):
-    folder_path = os.path.join(app.root_path, folder)  # Lấy đường dẫn đầy đủ đến thư mục
+@app.route('/images/<folder>/<subfolder>/<filename>', methods=['GET'])
+def get_image(folder, filename, subfolder=None):
+    if subfolder:
+        folder_path = os.path.join(app.root_path, folder, subfolder)
+    else:
+        folder_path = os.path.join(app.root_path, folder)
+    
     if os.path.exists(os.path.join(folder_path, filename)):
         return send_from_directory(folder_path, filename)
     else:
